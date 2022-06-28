dd74538fcb29698cb4254f7a7df75375f78b30e8
makepluscode <makepluscode@gmail.com>
2022-06-28 16:37:47 +0530

    Create symlinks

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,28 @@ int main() {
 
     for (const auto & device : devices) 
     {
-        std::cout << device.device_description <<  " at " << device.bus_info << " is attached to\n";
+        //std::cout << device.device_description <<  " at " << device.bus_info << " is attached to\n";
 
-        for (const auto & path : device.device_paths) {
-            std::cout << path << "\n";
+        for (const auto & path : device.device_paths)
+        {
+            auto res = device.device_description.find("ar0233");
+
+            if(device.device_description.find("ar0233") < 64)
+            {
+                std::cout << path.c_str() << " is NileCAM21\n";
+                if ( -1 == symlink( path.c_str(), "/home/nvidia/gmsl"))
+                {
+                    std::cout << path << " link created as \n";
+                }
+            }
+            else if(device.device_description.find("See3CAM_CU20") < 64)
+            {
+                std::cout << path.c_str() << " is See3CAM_CU20\n";
+                if ( -1 == symlink( path.c_str(), "/home/nvidia/usb"))
+                {
+                    std::cout << path << " link created as\n";
+                }
+            }
         }
 
         std::cout << "\n";
